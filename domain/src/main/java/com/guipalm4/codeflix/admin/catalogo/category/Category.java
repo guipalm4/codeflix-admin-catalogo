ca690a09b0886932793c4e65a67c51ad9de816e8
guipalm4 <gomes.lmc@gmail.com>
2022-04-27 20:04:42 -0300

    Transforma classe Category em um AggregateRoot

--- domain/src/main/java/com/guipalm4/codeflix/admin/catalogo/category/Category.java
+++ domain/src/main/java/com/guipalm4/codeflix/admin/catalogo/category/Category.java
@@ -1,11 +1,11 @@
 package com.guipalm4.codeflix.admin.catalogo.category;
 
+import com.guipalm4.codeflix.admin.catalogo.AggregateRoot;
+
 import java.time.Instant;
-import java.util.UUID;
 
-public class Category {
+public class Category extends AggregateRoot<CategoryID> {
 
-    private String id;
     private String name;
     private String description;
     private boolean active;
@@ -13,31 +13,31 @@ public class Category {
     private Instant updatedAt;
     private Instant deletedAt;
 
-    public Category(final String id,
-                    final String name,
-                    final String description,
-                    final boolean active,
-                    final Instant createdAt,
-                    final Instant updatedAt,
-                    final Instant deletedAt
+    public Category(final CategoryID aId,
+                    final String aName,
+                    final String aDescription,
+                    final boolean isActive,
+                    final Instant aCreationDate,
+                    final Instant aUpdateDate,
+                    final Instant aDeleteDate
     ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
+        super(aId);
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreationDate;
+        this.updatedAt = aUpdateDate;
+        this.deletedAt = aDeleteDate;
     }
 
     public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
-        final var id = UUID.randomUUID().toString();
+        final var id = CategoryID.unique();
         final var now = Instant.now();
 
         return new Category(id, aName, aDescription, isActive, now, now, null);
     }
 
-    public String getId() {
+    public CategoryID getId() {
         return id;
     }
 
