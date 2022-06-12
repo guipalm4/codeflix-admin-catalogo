1446d08b491a2370899b72856352670386f24dd9
guipalm4 <gomes.lmc@gmail.com>
2022-06-11 23:32:36 -0300

    Implementa notification pattern como um validation handler

--- application/src/main/java/com/guipalm4/codeflix/admin/catalogo/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/com/guipalm4/codeflix/admin/catalogo/category/create/DefaultCreateCategoryUseCase.java
@@ -2,6 +2,7 @@ package com.guipalm4.codeflix.admin.catalogo.category.create;
 
 import com.guipalm4.codeflix.admin.catalogo.category.Category;
 import com.guipalm4.codeflix.admin.catalogo.category.CategoryGateway;
+import com.guipalm4.codeflix.admin.catalogo.validation.handler.Notification;
 import com.guipalm4.codeflix.admin.catalogo.validation.handler.ThrowsValidationHandler;
 
 import java.util.Objects;
@@ -20,9 +21,15 @@ public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
         final var aDescription = aCommand.description();
         final var isActive = aCommand.isActive();
 
+        final var notification = Notification.create();
+
         final var aCategory = Category.newCategory(aName, aDescription, isActive);
 
-        aCategory.validate(new ThrowsValidationHandler());
+        aCategory.validate(notification);
+
+        if (notification.hasError()) {
+            //
+        }
 
         return CreateCategoryOutput.from(this.categoryGateway.create(aCategory));
     }
---
+++ domain/src/main/java/com/guipalm4/codeflix/admin/catalogo/validation/handler/Notification.java
@@ -0,0 +1,51 @@
+package com.guipalm4.codeflix.admin.catalogo.validation.handler;
+
+import com.guipalm4.codeflix.admin.catalogo.exceptions.DomainException;
+import com.guipalm4.codeflix.admin.catalogo.validation.Error;
+import com.guipalm4.codeflix.admin.catalogo.validation.ValidationHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notification implements ValidationHandler {
+
+    private final List<Error> errors;
+
+    private Notification(final List<Error> errors) {
+        this.errors = errors;
+    }
+
+    public static Notification create() {
+        return new Notification(new ArrayList<>());
+    }
+
+    @Override
+    public Notification append(final Error anError) {
+        this.errors.add(anError);
+        return this;
+    }
+
+    @Override
+    public Notification append(final ValidationHandler aHandler) {
+        this.errors.addAll(aHandler.getErrors());
+        return this;
+    }
+
+    @Override
+    public ValidationHandler validate(Validation aValidation) {
+        try{
+            aValidation.validate();
+
+        } catch (final DomainException ex) {
+            this.errors.addAll(ex.getErrors());
+        } catch (final Throwable ex) {
+            this.errors.add(new Error(ex.getMessage()));
+        }
+        return this;
+    }
+
+    @Override
+    public List<Error> getErrors() {
+        return this.errors;
+    }
+}
