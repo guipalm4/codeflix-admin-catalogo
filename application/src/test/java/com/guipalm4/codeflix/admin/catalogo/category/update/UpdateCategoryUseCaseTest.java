ac5837384e96341fccd011e736375a784c246cc8
guipalm4 <gomes.lmc@gmail.com>
2022-06-14 22:33:53 -0300

    Add demais cenários de teste o caso de uso de atualização de categoria

--- application/src/test/java/com/guipalm4/codeflix/admin/catalogo/category/update/UpdateCategoryUseCaseTest.java
+++ application/src/test/java/com/guipalm4/codeflix/admin/catalogo/category/update/UpdateCategoryUseCaseTest.java
@@ -2,6 +2,8 @@ package com.guipalm4.codeflix.admin.catalogo.category.update;
 
 import com.guipalm4.codeflix.admin.catalogo.category.Category;
 import com.guipalm4.codeflix.admin.catalogo.category.CategoryGateway;
+import com.guipalm4.codeflix.admin.catalogo.category.CategoryID;
+import com.guipalm4.codeflix.admin.catalogo.exceptions.DomainException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -69,4 +71,107 @@ public class UpdateCategoryUseCaseTest {
         ));
     }
 
+    @Test
+    public void givenAValidCommandWithInvalidParams_whenCallsCreateCategory_thenShouldReturnDomainException() {
+
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorsCount = 1;
+
+        final var aCategory = Category.newCategory("Film", "", true);
+        final var expectedId = aCategory.getId();
+
+        final var aCommand =
+                UpdateCategoryCommand.with(
+                        expectedId.getValue(),
+                        expectedName,
+                        expectedDescription,
+                        expectedActive);
+
+        when(gateway.findById(eq(expectedId)))
+                .thenReturn(Optional.of(Category.clone(aCategory)));
+
+        final var notification = useCase.execute(aCommand).getLeft();
+
+        Assertions.assertEquals(expectedErrorsCount, notification.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+        Mockito.verify(gateway, times(0)).update(any());
+    }
+
+    @Test
+    public void givenAValidCommandWithInactiveCategory_whenCallsUpdateCategory_thenReturnCategoryId() {
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedActive = false;
+
+        final var aCategory = Category.newCategory("Film", "", true);
+
+        final var expectedId = aCategory.getId();
+
+        final var aCommand = UpdateCategoryCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedDescription,
+                expectedActive
+        );
+
+        Assertions.assertTrue(aCategory.isActive());
+        Assertions.assertNull(aCategory.getDeletedAt());
+
+        when(gateway.findById(eq(expectedId)))
+                .thenReturn(Optional.of(Category.clone(aCategory)));
+
+        when(gateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand).get();
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        Mockito.verify(gateway, times(1)).findById(eq(expectedId));
+        Mockito.verify(gateway, times(1)).update(argThat(
+                aUpdateCategory ->
+                        Objects.equals(expectedName, aUpdateCategory.getName())
+                                && Objects.equals(expectedDescription, aUpdateCategory.getDescription())
+                                && Objects.equals(expectedActive, aUpdateCategory.isActive())
+                                && Objects.equals(expectedId, aUpdateCategory.getId())
+                                && Objects.equals(aCategory.getCreatedAt(), aUpdateCategory.getCreatedAt())
+                                && aCategory.getUpdatedAt().isBefore(aUpdateCategory.getUpdatedAt())
+                                && Objects.nonNull(aUpdateCategory.getDeletedAt())
+        ));
+    }
+
+    @Test
+    public void givenAValidCommandWithInvalidID_whenCallsUpdateCategory_thenShouldReturnDomainException() {
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedActive = true;
+        final var expectedErrorMessage = "Category with ID 123 was not found";
+        final var expectedErrorsCount = 1;
+        final var expectedId = CategoryID.from("123");
+
+        final var aCommand = UpdateCategoryCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedDescription,
+                expectedActive
+        );
+
+        when(gateway.findById(eq(expectedId)))
+                .thenReturn(Optional.empty());
+
+        final var expectedException = Assertions.assertThrows(
+                DomainException.class, () -> useCase.execute(aCommand));
+
+        Mockito.verify(gateway, times(1)).findById(eq(expectedId));
+        Assertions.assertEquals(expectedErrorsCount, expectedException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, expectedException.getErrors().get(0).message());
+        Mockito.verify(gateway, times(0)).update(any());
+    }
 }
+
