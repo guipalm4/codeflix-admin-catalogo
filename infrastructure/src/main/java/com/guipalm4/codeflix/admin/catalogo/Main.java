56ffb0cc7ccc57f2b213a416332fcdd3704af5bd
guipalm4 <gomes.lmc@gmail.com>
2022-06-15 22:50:00 -0300

    Add o springboot na camada de infrastructure

--- infrastructure/src/main/java/com/guipalm4/codeflix/admin/catalogo/Main.java
+++ infrastructure/src/main/java/com/guipalm4/codeflix/admin/catalogo/Main.java
@@ -1,9 +1,13 @@
 package com.guipalm4.codeflix.admin.catalogo;
 
+
+import com.guipalm4.codeflix.admin.catalogo.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
--- infrastructure/src/main/java/com/guipalm4/codeflix/admin/catalogo/util/mapper/DTOModelMapper.java
+++
@@ -1,6 +0,0 @@
-package com.guipalm4.codeflix.admin.catalogo.util.mapper;
-
-public class DTOModelMapper {
-    //TODO implementar após add spring no projeto
-    //REF: https://auth0.com/blog/automatically-mapping-dto-to-entity-on-spring-boot-apis/
-}
