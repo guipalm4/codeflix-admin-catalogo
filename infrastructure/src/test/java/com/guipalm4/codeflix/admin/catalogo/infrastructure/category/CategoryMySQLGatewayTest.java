94f3e0f24131ac4102e99096153700a1b4cd4624
guipalm4 <gomes.lmc@gmail.com>
2022-06-18 11:37:13 -0300

    Add integrados do método deletebyid do gateway de categoria

--- infrastructure/src/test/java/com/guipalm4/codeflix/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/guipalm4/codeflix/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,6 +1,7 @@
 package com.guipalm4.codeflix.admin.catalogo.infrastructure.category;
 
 import com.guipalm4.codeflix.admin.catalogo.domain.category.Category;
+import com.guipalm4.codeflix.admin.catalogo.domain.category.CategoryID;
 import com.guipalm4.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import com.guipalm4.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.guipalm4.codeflix.admin.catalogo.infrastructure.config.MySQLGatewayTest;
@@ -99,5 +100,36 @@ public class CategoryMySQLGatewayTest {
         Assertions.assertEquals(aCategory.getDeletedAt(), actualEntity.getDeletedAt());
         Assertions.assertNull(actualEntity.getDeletedAt());
     }
+
+    @Test
+    public void givenAPrePersistedCategoryAndValidCategoryId_whenTryToDeleteIt_shouldDeleteCategory() {
+        final var aCategory = Category.newCategory("Filmes", null, true);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        categoryRepository.saveAndFlush(CategoryJpaEntity.from(aCategory));
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        categoryGateway.deleteById(aCategory.getId());
+
+        Assertions.assertEquals(0, categoryRepository.count());
+    }
+
+    @Test
+    public void givenInvalidCategoryId_whenTryToDeleteIt_shouldNotDeleteCategory() {
+        final var aCategory = Category.newCategory("Filmes", null, true);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        categoryRepository.saveAndFlush(CategoryJpaEntity.from(aCategory));
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        categoryGateway.deleteById(CategoryID.from("invalid"));
+
+        Assertions.assertEquals(1, categoryRepository.count());
+    }
+
 
 }
